8809a7f56e00bb81662f3b7e2f8c1bda9508299d
Pedro H. C <pedrohcm5@gmail.com>
2021-02-01 21:04:51 -0300

    p0_3.py added

---
+++ set0/problemas/p0_3.py
@@ -0,0 +1,13 @@
+kwh_used = 1800
+
+
+if kwh_used <= 500:
+    out = (0.45 * kwh_used) * 1.2
+elif kwh_used <= 1500:
+    out = ((0.45 * 500) + ((kwh_used - 500) * 0.74)) * 1.2
+elif kwh_used <= 2500:
+    out = ((0.45 * 500) + ((kwh_used - 500) * 0.74) + (kwh_used - 1500) * 1.25) * 1.2
+else:
+    out = ((0.45 * 500) + ((kwh_used - 500) * 0.74) + ((kwh_used - 1500) * 1.25) + ((kwh_used - 2500)) * 2) * 1.2
+
+print(out)
