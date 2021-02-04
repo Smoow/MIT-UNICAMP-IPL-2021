dc03baf01054e67ff3b83f24de3317f94bf29126
Pedro H. C <pedrohcm5@gmail.com>
2021-02-03 23:03:27 -0300

    p1_6.py updated

--- set1/problemas/p1_6.py
+++ set1/problemas/p1_6.py
@@ -8,26 +8,13 @@ counter = 0
 len_neg = int(((p_d - 1) / 2) - (p_d - 1))
 len_pos = int(((p_d - 1) / 2))
 
-for i in range(len_neg, 1, 1):
-    for j in range(len_neg, 1, 1):
-        generator.append([i, j])
-
-for i in range(1, len_pos+1, 1):
-    for j in range(1, len_pos+1, 1):
-        generator.append([i, j])
-
-for i in range(1, len_pos+1, 1):
-    for j in range(len_neg, 1, 1):
-        generator.append([i, j])
-
-for i in range(len_neg, 1, 1):
-    for j in range(1, len_pos+1, 1):
+for i in range(len_neg, len_pos+1, 1):
+    for j in range(len_neg, len_pos+1, 1):
         generator.append([i, j])
 
 while (counter < len(generator)):
     if( (((generator[counter][0] ** 2) + generator[counter][1] ** 2) ** 0.5) <= p_d/2 ): 
         circles_inside.append(generator[counter])
-    # print(counter)
     counter += 1
 
 out = (len(circles_inside)/total_area) * 4
