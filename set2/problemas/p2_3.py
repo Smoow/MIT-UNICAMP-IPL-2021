22427e7783dafc6de853d0d713e26917333c9315
Pedro H. C <pedrohcm5@gmail.com>
2021-02-05 00:06:14 -0300

    p2_2.py added

---
+++ set2/problemas/p2_3.py
@@ -0,0 +1,18 @@
+def ndoors(doors):
+    doors_locked = [i for i in range(1, doors+1)]
+    doors_unlocked = []
+
+    for i in range(1, doors+1):
+        for j in range(1, doors+1):
+            if (j % i == 0):
+                if j in doors_locked:
+                    doors_locked.remove(j)
+                    doors_unlocked.append(j)
+                else:
+                    doors_locked.append(j)
+                    doors_unlocked.remove(j)
+                    
+    return doors_unlocked
+
+
+print(ndoors(1000))
