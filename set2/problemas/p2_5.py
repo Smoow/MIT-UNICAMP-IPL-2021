f72945419a931a15530dba1e40adc5a6a4e54799
Smoow <pedrohcm5@gmail.com>
2021-02-05 01:02:57 -0300

    p2_5 refactored

--- set2/problemas/p2_5.py
+++ set2/problemas/p2_5.py
@@ -4,9 +4,11 @@ alpha = string.ascii_lowercase
 digits = string.digits
 
 def caesar_cipher(str, shift):
-    """ The Caesar Cipher is a famous and very old cryptography technique. 
+    """ 
+        The Caesar Cipher is a famous and very old cryptography technique. 
         In a simple way, it reorganize all the letter from a sentence based on a shifted alphabet.
-        For example, if we choose a right shift of 4, the letter A is replaced by E, B is replaced by F, and so on. 
+        For example, if we choose a right shift of 4, the letter A is replaced by E, 
+        B is replaced by F, and so on. 
         When the end of alphabet or decimal numbers is reached, it returns back to the beginning.
     """
     ciphered = ""
@@ -17,21 +19,15 @@ def caesar_cipher(str, shift):
             index = alpha.find(low_letter)
             index += shift
 
-            if (index > 25):
-                index %= 26
-                
-            elif (index < 0):
-                index %= 26
-                
+            if (index > 25 or index < 0): index %= 26
+
             ciphered += alpha[abs(index)]
 
         elif i.isdecimal():
             index = digits.find(i)
             index += shift
-            if index > 9:
-                index %= 10
-            elif index < 0:
-                index %= 10
+
+            if (index > 9 or index < 0): index %= 10
 
             ciphered += digits[abs(index)]
 
