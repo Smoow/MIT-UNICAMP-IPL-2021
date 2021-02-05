9e5f90c6c286056b2976b1fade626ca526dddeba
Pedro H. C <pedrohcm5@gmail.com>
2021-02-05 00:05:47 -0300

    p2_1.py added

---
+++ set2/problemas/p2_1.py
@@ -0,0 +1,15 @@
+import math
+
+def square(x):
+    return x * x
+
+def fourth_power(x):
+    return square(square(x))
+
+def perfect_square(x):
+    sqrt = math.sqrt(x)
+
+    if ((int(sqrt + 0.5) ** 2) == x):
+        return True
+    else:
+        return False
