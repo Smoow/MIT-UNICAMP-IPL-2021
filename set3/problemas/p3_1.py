523504f1c377b85f14b3d6e7ee077cc3948d2c79
Pedro H. C <pedrohcm5@gmail.com>
2021-02-06 13:26:46 -0300

    p3_1 added

---
+++ set3/problemas/p3_1.py
@@ -0,0 +1,30 @@
+def join_arrays(array):
+    """
+        Returns a joint of any arrays to a single one
+    """
+    out = []
+    for i in array:
+        for j in i:
+            out.append(j)
+
+    out.append(None)
+    return out
+
+def run_length_encode_2d(array):
+    joined_array = join_arrays(array)
+    array_length = len(joined_array)
+    counter_index = 1
+    actual = 0
+    counter = 1
+    out = []
+    
+    while (counter_index < array_length):
+        if joined_array[actual] == joined_array[counter_index]:
+            counter += 1
+        else:
+            out.append((counter, joined_array[actual]))
+            actual += counter
+            counter = 1
+        counter_index += 1
+    
+    return out
