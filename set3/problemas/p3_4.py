7896696e858d51a51fdf2bdf16ddade273a62572
Pedro H. C <pedrohcm5@gmail.com>
2021-02-09 18:54:22 -0300

    refactor p3_4

--- set3/problemas/p3_4.py
+++ set3/problemas/p3_4.py
@@ -1,26 +1,20 @@
 def lend_money(debts, person, amount):
-    flag = 0
-    for key in debts:
-        if key == person:
-            flag = 1
-            debts[person].append(amount)
+    if person in debts:
+        debts[person].append(amount)
+    else:
+        debts[person] = [amount]
 
-    if (flag == 0): debts[person] = [amount]
-        
     return None
 
 
 def amount_owed_by(debts, person):
-    flag = 0
     s_v = 0
-    for key in debts:
-        if key == person:
-            flag = 1
-            for values in debts[person]:
-                s_v += values
-            return s_v
-
-    if (flag == 0): return 0
+    if person in debts:
+        for values in debts[person]:
+            s_v += values
+        return s_v
+    else:
+        return 0
 
 
 def total_amount_owed(debts):
@@ -30,5 +24,3 @@ def total_amount_owed(debts):
             total += values
 
     return total
-
-debts = {}
