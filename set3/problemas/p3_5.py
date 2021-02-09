60f10a7a1c61abe2e4190e1525e11a8b75df0d41
Pedro H C <pedrohcm5@gmail.com>
2021-02-08 21:10:29 -0300

    p3_5 first version without recursion

--- set3/problemas/p3_5.py
+++ set3/problemas/p3_5.py
@@ -1,7 +1,7 @@
 def approx_derivative(f, x, delta=1e-6):
     derivative = (f(x+delta) - f(x-delta)) / (2*delta)
     return derivative
-
+    
 
 def approx_derivative_2(f, delta=1e-6):
     def f_line(x):
@@ -24,3 +24,6 @@ def approx_integral(f, lo, hi, num_regions):
 
 def f(x):
     return x * x
+
+
+print(approx_integral(f, 1, 5, 2))
